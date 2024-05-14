a50960d7d9d507db0ae106dc6de0f4b75b2cdeb1
carolusquintus <carloschars@gmail.com>
2024-05-13 22:30:30 -0600

    Creating DataSourceInit bean with data.sql script

--- src/main/java/dev/carm/sistemaescolar/config/AppConfig.java
+++ src/main/java/dev/carm/sistemaescolar/config/AppConfig.java
@@ -2,7 +2,12 @@ package dev.carm.sistemaescolar.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
@@ -10,6 +15,16 @@ public class AppConfig {
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        var populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("/data.sql"));
+        var initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(populator);
+        return initializer;
     }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO menu (id, parent_id, name, path, active)
+VALUES (1, 0, 'Actividades', '/activities', true);
+
+INSERT INTO menu (id, parent_id, name, path, active)
+VALUES (2, 0, 'Administración', '/admin', false);
